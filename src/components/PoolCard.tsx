767205ad836343ed94ecf3dbee09b71d8475bbbb
maazramailo <maaz@ramailo.tech>
2024-06-12 15:27:13 +0530

    prize page

--- src/components/PoolCard.tsx
+++ src/components/PoolCard.tsx
@@ -1,8 +1,7 @@
-import { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import RectangleImage from "../images/rectangle.svg";
 import BarImage from "../images/bar_img.svg";
-
+import Timer from "./Timer";
 const PoolContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -125,8 +124,6 @@ interface TimeLeft {
   seconds: number;
 }
 
-const formatTime = (time: number) => String(time).padStart(2, "0");
-
 interface PoolCardProps {
   initialTime: TimeLeft;
   middleContentBgColor: string;
@@ -146,57 +143,6 @@ const PoolCard: React.FC<PoolCardProps> = ({
   tagColor,
   tagBgColor,
 }) => {
-  const [timeLeft, setTimeLeft] = useState<TimeLeft>(initialTime);
-  const countdown = useCallback(() => {
-    setTimeLeft((prevTimeLeft) => {
-      const { days, hours, minutes, seconds } = prevTimeLeft;
-      if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
-        return prevTimeLeft;
-      }
-
-      let newDays = days,
-        newHours = hours,
-        newMinutes = minutes,
-        newSeconds = seconds - 1;
-
-      if (newSeconds < 0) {
-        newSeconds = 59;
-        newMinutes -= 1;
-      }
-      if (newMinutes < 0) {
-        newMinutes = 59;
-        newHours -= 1;
-      }
-      if (newHours < 0) {
-        newHours = 23;
-        newDays -= 1;
-      }
-
-      return {
-        days: newDays,
-        hours: newHours,
-        minutes: newMinutes,
-        seconds: newSeconds,
-      };
-    });
-  }, []);
-
-  useEffect(() => {
-    const timerId = setInterval(countdown, 1000);
-    return () => clearInterval(timerId);
-  }, [countdown]);
-
-  const renderTime = (time: string) => (
-    <>
-      <p className="bg-[#181F271A] px-[4px] py-[2px] max-w-max rounded-[4px]">
-        {time[0]}
-      </p>
-      <p className="bg-[#181F271A] px-[4px] py-[2px] max-w-max rounded-[4px]">
-        {time[1]}
-      </p>
-    </>
-  );
-
   return (
     <PoolContainer>
       <MainBody>
@@ -225,23 +171,7 @@ const PoolCard: React.FC<PoolCardProps> = ({
             <p className="text-xs leading-normal font-medium text-[#181F27]">
               will be awarded
             </p>
-            <div className="flex flex-col">
-              <div className="text-sm leading-normal text-[#181F27] font-semibold flex gap-[2px]">
-                {renderTime(formatTime(timeLeft.days))}
-                <p className="px-[4px] py-[2px] max-w-max">:</p>
-                {renderTime(formatTime(timeLeft.hours))}
-                <p className="px-[4px] py-[2px] max-w-max">:</p>
-                {renderTime(formatTime(timeLeft.minutes))}
-                <p className="px-[4px] py-[2px] max-w-max">:</p>
-                {renderTime(formatTime(timeLeft.seconds))}
-              </div>
-              <div className="flex justify-between text-[#181F27] text-xs font-normal leading-normal">
-                <p className="px-2">DAY</p>
-                <p className="px-2">HR</p>
-                <p className="px-2">MIN</p>
-                <p className="px-2">SEC</p>
-              </div>
-            </div>
+            <Timer initialTime={initialTime} />
           </div>
         </MiddleContent>
         <LowerContent>
